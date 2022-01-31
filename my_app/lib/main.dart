ad602b1fb49984133450b781331ee4bcbb1a7241
Udit <udgupta155@gmail.com>
2022-01-31 20:29:39 +0530

    Commit

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   var message = "My First Application";
   var image = "assets/images/Acer.jpg";
+  var msgForNav = "Hello World";
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     // home: Text("Hello World"), // Start Blank
     home: Scaffold(
       drawer: Drawer(
         backgroundColor: Colors.amber,
-        child: Center(child: Text("Hello World")),
+        child: Center(child: Text(msgForNav)),
       ),
       appBar: AppBar(
         title: Text(message),
