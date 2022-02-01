f913d993542623bb5bbcb9dc80c4d88a58cd0c06
Udit <udgupta155@gmail.com>
2022-02-01 13:37:13 +0530

    Commit

---
+++ my_app/Learning Backup/Day 2/Without_Cards.dart
@@ -0,0 +1,102 @@
+// Here I have used Container but the best way is Cards
+
+import 'package:flutter/material.dart';
+
+var name = "Udit Gupta";
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Image(image: AssetImage("assets/images/Acer.jpg")),
+            CircleAvatar(
+              backgroundColor: Colors.amber,
+              radius: 50.0,
+              backgroundImage: AssetImage("assets/images/Ud.jpg"),
+            ),
+            Text(
+              name,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+            Text(
+              "CONFLUENT DEVELOPER",
+              style: TextStyle(
+                color: Colors.teal.shade100,
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'SourceSansPro',
+                letterSpacing: 2,
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.phone,
+                    size: 25,
+                    color: Colors.teal.shade300,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "+91 9981335244",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.teal,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                    size: 25,
+                    color: Colors.teal.shade300,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "udgupta155@gmail.com",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.teal,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              child: Text("Data"),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
